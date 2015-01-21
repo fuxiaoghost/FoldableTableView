0ba06a8e3a99ec417898bc1033b2cc4726e36c9f
Kirn <wangsguang@gmail.com>
2015-01-21 23:01:50 +0800

    new project

---
+++ FoldableTableView/FoldableTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoldableTableView
+//
+//  Created by Kirn on 15/1/21.
+//  Copyright (c) 2015年 kirn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoldableTableView/FoldableTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoldableTableView
+//
+//  Created by Kirn on 15/1/21.
+//  Copyright (c) 2015年 kirn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
